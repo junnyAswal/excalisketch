b0ff6ec1faad68f5b81109d0df0fcb0e40e31de8
Arjun <165657057+junnyAswal@users.noreply.github.com>
2025-08-17 19:49:27 +1000

    created style panel component

---
+++ src/components/StylePanel.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export function StylePanel() {
+    // create a style object
+    // edit pen and rectangle tool + live drawing to use the colours from style object
+    // take in style prop
+    // create menu with sliders
+    // return the updated style object
+
+    return;
+}
