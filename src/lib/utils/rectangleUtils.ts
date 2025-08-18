479a0677404ab7d6aa91d68ce20b8d8c45f4f670
Arjun <165657057+junnyAswal@users.noreply.github.com>
2025-08-18 20:43:35 +1000

    updated rectangle styling

--- src/lib/utils/rectangleUtils.ts
+++ src/lib/utils/rectangleUtils.ts
@@ -1,11 +1,12 @@
 import { RefObject } from "react";
-import { Point, Rectangle } from "@/types";
+import { Point, Rectangle, StrokeWidth, Style } from "@/types";
 
 export function updateDynamicRectangle(
     ref: RefObject<SVGRectElement | null>,
     points: Point[],
     currX: number,
-    currY: number
+    currY: number,
+    styleSettings: Style
 ) {
     if (!ref.current) return;
 
@@ -19,28 +20,37 @@ export function updateDynamicRectangle(
     ref.current.setAttribute("y", `${y}`);
     ref.current.setAttribute("width", `${width}`);
     ref.current.setAttribute("height", `${height}`);
-    ref.current.setAttribute("fill", "none");
-    ref.current.setAttribute("stroke", "black");
-    ref.current.setAttribute("rx", "20");
-    ref.current.setAttribute("ry", "20");
-    ref.current.style.strokeWidth = "10";
+    ref.current.setAttribute("stroke", `${styleSettings.strokeColour}`);
+
+    const radius = styleSettings.rounded ? 10 : 0;
+
+    ref.current.setAttribute("rx", `${radius}`);
+    ref.current.setAttribute("ry", `${radius}`);
+    ref.current.setAttribute("fill", styleSettings.fillColour);
+    ref.current.style.strokeWidth = `${styleSettings.strokeWidth}`;
 }
 
 export function createRectangle(
     points: Point[],
     currX: number,
-    currY: number
+    currY: number,
+    styleSettings: Style
 ): Rectangle {
     const startPoints = points[0];
     const x = Math.min(startPoints[0], currX);
     const y = Math.min(startPoints[1], currY);
     const width = Math.abs(currX - startPoints[0]);
     const height = Math.abs(currY - startPoints[1]);
+    const radius = styleSettings.rounded ? 10 : 0;
 
     return {
         x: x,
         y: y,
         width: width,
         height: height,
+        radius: radius,
+        strokeWidth: styleSettings.strokeWidth,
+        strokeColour: styleSettings.strokeColour,
+        fillColour: styleSettings.fillColour,
     };
 }
