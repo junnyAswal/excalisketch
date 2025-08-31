ab22b268490c32f4b53ec1f05f9aa2b8b5cea134
Arjun <165657057+junnyAswal@users.noreply.github.com>
2025-08-31 19:37:48 +1000

    fixed any type

--- src/lib/utils/typeGuards.ts
+++ src/lib/utils/typeGuards.ts
@@ -1,9 +1,14 @@
 import { Rectangle, Sketch } from "@/types";
 
-export function isSketch(obj: any): obj is Sketch {
-    return "strokes" in obj;
+export function isSketch(obj: unknown): obj is Sketch {
+    return typeof obj === "object" && obj !== null && "strokes" in obj;
 }
 
-export function isRectangle(obj: any): obj is Rectangle {
-    return "shape" in obj && obj.shape === "rectangle";
+export function isRectangle(obj: unknown): obj is Rectangle {
+    return (
+        typeof obj === "object" &&
+        obj !== null &&
+        "shape" in obj &&
+        obj.shape === "rectangle"
+    );
 }
